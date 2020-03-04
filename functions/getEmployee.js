41f47637f0779e27906b3fb8328b4ea732426bed
Fernando Diaz Ramos <FDiaz13@outlook.com>
2020-03-04 08:44:21 -0600

    restructuring response

--- functions/getEmployee.js
+++ functions/getEmployee.js
@@ -1,12 +1,18 @@
 const axios = require('axios');
-
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 const API_ENDPOINT = process.env.API_ENDPOINT;
 
 exports.handler = (event, context, callback) => {
   axios.get(API_ENDPOINT).then(res => {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(res.data),
+      body: JSON.stringify({
+        _id: res.data.data._id,
+        name: res.data.data.employee_name,
+        position: '',
+        office: res.data.data.employee_age,
+        salary: res.data.data.employee_salary,
+      }),
     });
   }).catch(err => {
     callback(err, {
