6dff0657a9b4d5297e9bec2bc155c06bbcf9848f
Fernando Diaz Ramos <FDiaz13@outlook.com>
2020-03-03 22:08:55 -0600

    axios without default

--- functions/getEmployee.js
+++ functions/getEmployee.js
@@ -1,25 +1,19 @@
-const axios = require('axios').default;
+const axios = require('axios');
 
-const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos';
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
-exports.handler = async (event, context) => {
-  let response
-  try {
-    response = await axios.get(API_ENDPOINT);
-    // handle response
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
+exports.handler = (event, context, callback) => {
+  axios.get(API_ENDPOINT).then(res => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(res.data),
+    });
+  }).catch(err => {
+    callback(err, {
+      statusCode: err.status || 500,
       body: JSON.stringify({
         error: err.message
-      })
-    }
-  }
-
-  return {
-    statusCode: 200,
-    body: {
-      data: response.data
-    }
-  }
-}
+      }),
+    });
+  })
+};
